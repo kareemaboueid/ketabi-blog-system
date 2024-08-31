1273f742c7fb72e621c55f530f11e9f3fa8b9b48
kareemaboueid <kareem_aboueid@outlook.com>
2024-08-31 23:11:24 +0300

    chore: Update MongoDB connection and server setup

--- src/index.js
+++ src/index.js
@@ -1,38 +1,60 @@
-const mongoose = require('mongoose');
-const app = require('./app');
-const config = require('./config/config');
-const logger = require('./config/logger');
+import mongoose from 'mongoose';
+import app from './app.js';
+import { env_vars } from './config/env-vars.cnfg.js';
+import logger from './config/logger.cnfg.js';
 
+// define the server
 let server;
-mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
-  logger.info('Connected to MongoDB');
-  server = app.listen(config.port, () => {
-    logger.info(`Listening to port ${config.port}`);
-  });
-});
 
-const exitHandler = () => {
+// define the exit process handler
+const exit_process_handler = () => {
   if (server) {
     server.close(() => {
       logger.info('Server closed');
       process.exit(1);
     });
-  } else {
-    process.exit(1);
-  }
+  } else process.exit(1);
 };
 
-const unexpectedErrorHandler = (error) => {
+// define the unexpected error handler
+const unexpected_error_handler = (error) => {
   logger.error(error);
-  exitHandler();
+  exit_process_handler();
 };
 
-process.on('uncaughtException', unexpectedErrorHandler);
-process.on('unhandledRejection', unexpectedErrorHandler);
+// define the MongoDB URI
+const db_uri = env_vars.mongoose.url
+  .replace('%USERNAME%', env_vars.mongoose.username)
+  .replace('%PASSWORD%', env_vars.mongoose.password)
+  .replace(
+    '%DBNAME%',
+    env_vars.env === 'development'
+      ? env_vars.mongoose.test_db
+      : env_vars.mongoose.real_db,
+  );
 
+// connect to MongoDB
+mongoose
+  .connect(db_uri)
+  .then(() => {
+    logger.info(`Connected to MongoDB [${mongoose.connection.name}]`);
+    server = app.listen(env_vars.port, () => {
+      logger.info(`Server listening [${env_vars.port}]`);
+    });
+  })
+  .catch((error) => {
+    logger.error(`Failed to connect to MongoDB: ${error}`);
+    exit_process_handler();
+  });
+
+// define the process event listeners on 'uncaughtException'
+process.on('uncaughtException', unexpected_error_handler);
+
+// define the process event listeners on 'unhandledRejection'
+process.on('unhandledRejection', unexpected_error_handler);
+
+// define the process event listeners on 'SIGTERM'
 process.on('SIGTERM', () => {
   logger.info('SIGTERM received');
-  if (server) {
-    server.close();
-  }
+  if (server) server.close();
 });
