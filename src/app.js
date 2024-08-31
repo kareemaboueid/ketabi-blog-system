4a8a36a8402ec3856626882f9465a3097b4ea4e0
kareemaboueid <kareem_aboueid@outlook.com>
2024-08-31 23:11:17 +0300

    chore: Update app.js imports and middleware configuration

--- src/app.js
+++ src/app.js
@@ -1,24 +1,23 @@
-const express = require('express');
-const helmet = require('helmet');
-const xss = require('xss-clean');
-const mongoSanitize = require('express-mongo-sanitize');
-const compression = require('compression');
-const cors = require('cors');
-const passport = require('passport');
-const httpStatus = require('http-status');
-const config = require('./config/config');
-const morgan = require('./config/morgan');
-const { jwtStrategy } = require('./config/passport');
-const { authLimiter } = require('./middlewares/rateLimiter');
-const routes = require('./routes/v1');
-const { errorConverter, errorHandler } = require('./middlewares/error');
-const ApiError = require('./utils/ApiError');
+import express from 'express';
+import expressEjsLayouts from 'express-ejs-layouts';
+import helmet from 'helmet';
+import mongoSanitize from 'express-mongo-sanitize';
+import cors from 'cors';
+import { StatusCodes } from 'http-status-codes';
+import { log_success, log_error } from './config/morgan.cnfg.js';
+import { env_vars } from './config/env-vars.cnfg.js';
+import Api_error from './utilities/api-error.util.js';
+import {
+  error_converter,
+  error_handler,
+} from './middlewares/server_error.mw.js';
+import v1 from './routes/v1/v1.js';
 
 const app = express();
 
-if (config.env !== 'test') {
-  app.use(morgan.successHandler);
-  app.use(morgan.errorHandler);
+if (env_vars.env !== 'development') {
+  app.use(log_success);
+  app.use(log_error);
 }
 
 // set security HTTP headers
@@ -31,37 +30,37 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // sanitize request data
-app.use(xss());
 app.use(mongoSanitize());
-
-// gzip compression
-app.use(compression());
 
 // enable cors
 app.use(cors());
 app.options('*', cors());
 
-// jwt authentication
-app.use(passport.initialize());
-passport.use('jwt', jwtStrategy);
+// set static folder
+app.use(express.static('public'));
 
-// limit repeated failed requests to auth endpoints
-if (config.env === 'production') {
-  app.use('/v1/auth', authLimiter);
-}
+// enable expressEjsLayouts
+app.use(expressEjsLayouts);
+
+// set layout file
+app.set('layout', 'layouts/main');
+
+// set view engine
+app.set('view engine', 'ejs');
 
 // v1 api routes
-app.use('/v1', routes);
+app.use('/v1', v1);
 
 // send back a 404 error for any unknown api request
-app.use((req, res, next) => {
-  next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
+app.use((_request, _response, _next) => {
+  _next(new Api_error(StatusCodes.NOT_FOUND, 'Route not found'));
 });
 
-// convert error to ApiError, if needed
-app.use(errorConverter);
+// convert error to Api_error, if needed
+app.use(error_converter);
 
 // handle error
-app.use(errorHandler);
+app.use(error_handler);
 
-module.exports = app;
+// export the app
+export default app;
